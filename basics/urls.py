440bf5db52be14017a19c525157b4a5e7e297e3c
PeterMagaga <ptrmagaga@gmail.com>
2025-02-21 03:38:56 +1400

    Fixed URL configuration

--- basics/urls.py
+++ basics/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
+def home(request):
+    return HttpResponse("Hello Django is running")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
     path('api/',include('trial.urls')),
     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
